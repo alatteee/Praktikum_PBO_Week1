32479db6d5b39ae8e6c9746f7066e8fb7dc292fa
Azkha Nazzala <azkhanazzala@gmail.com>
2025-08-24 11:17:30 +0700

    mengerjakan soal nomer 1 dan 2

---
+++ DataTypes.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.math.*;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // jumlah test case
+
+        for (int i = 0; i < T; i++) {
+            try {
+                long n = sc.nextLong(); // coba baca input sebagai long
+                System.out.println(n + " can be fitted in:");
+                
+                if (n >= Byte.MIN_VALUE && n <= Byte.MAX_VALUE)
+                    System.out.println("* byte");
+                if (n >= Short.MIN_VALUE && n <= Short.MAX_VALUE)
+                    System.out.println("* short");
+                if (n >= Integer.MIN_VALUE && n <= Integer.MAX_VALUE)
+                    System.out.println("* int");
+                if (n >= Long.MIN_VALUE && n <= Long.MAX_VALUE)
+                    System.out.println("* long");
+
+            } catch (Exception e) {
+                // kalau angkanya lebih besar dari long
+                String num = sc.next(); 
+                System.out.println(num + " can't be fitted anywhere.");
+            }
+        }
+        sc.close();
+    }
+}
+
+
